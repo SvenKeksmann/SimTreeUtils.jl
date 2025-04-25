c2db0c9f1a042f64d70c44348090cbf2a52ab9e6
svnwrts <st172247@stud.uni-stuttgart.de>
2025-04-25 17:38:24 +0200

    simlog TestFunktion entfernt

--- src/simlog.jl
+++ src/simlog.jl
@@ -1,15 +1,3 @@
-
-function simlog(logmsg)
-    #logtime=Dates.Dates.format(now(), "yyyy-mm-dd HH:MM:SS.sss");
-    
-    #logline = string("[", logtime, "] - ", logmsg)
-    #logline = string("[", "####", "] - ", logmsg)
-    #println(logline)
-
-    #logger = LokiLogger.Logger(LokiLogger.json, "http://localhost:3100";
-    #    labels=Dict("datacenter" => "eu-north", "app" => "SimTreeUtils"))
-end
-
 function simlogint() 
     return LokiLogger.Logger(LokiLogger.json, "http://localhost:3100"; labels=Dict("host" => gethostname(), "app" => "LokiLogger.jl"))
 end
